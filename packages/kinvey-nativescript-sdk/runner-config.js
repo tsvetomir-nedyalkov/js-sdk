aa4c344726d3d849db45af2fe2bb666d5a275960
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2018-01-08 11:02:13 +0200

    Removed a not needed command, which breaks the build on NS - iOS

--- packages/kinvey-nativescript-sdk/runner-config.js
+++ packages/kinvey-nativescript-sdk/runner-config.js
@@ -78,14 +78,6 @@ const runner = new Runner({
         }),
         copyTestRunner(appPath),
         runCommand({
-            command: 'adb',
-            args: [
-                'reverse',
-                () => `tcp:${logServerPort}`,
-                () => `tcp:${logServerPort}`
-            ]
-        }),
-        runCommand({
             command: 'tns',
             args: ['run', osName, '--justlaunch'],
             cwd: appRootPath
