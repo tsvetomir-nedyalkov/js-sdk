fdc5a67347a9cb1efb973b0d77bb475652de112b
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2018-01-17 18:45:36 +0200

    added cordova run back in runner-config

--- packages/kinvey-phonegap-sdk/runner-config.js
+++ packages/kinvey-phonegap-sdk/runner-config.js
@@ -1,21 +1,19 @@
 const path = require('path');
 const walk = require('klaw-sync');
-const fs = require('fs-extra');
 
 const {
-    Runner,
-    tasks: {
-        logServer,
-        copy,
-        copyTestRunner,
-        copyTestLibs,
-        runCommand,
-        remove,
-        processTemplateFile
-    },
-    conditionals: {
-        when
-    }
+  Runner,
+  tasks: {
+    logServer,
+    copy,
+    copyTestRunner,
+    runCommand,
+    remove,
+    processTemplateFile
+  },
+  conditionals: {
+    when
+  }
 } = require('kinvey-universal-runner');
 
 const appName = 'KinveyCordovaTestApp';
@@ -30,57 +28,82 @@ let logServerPort;
 
 
 function runPipeline(osName) {
-
-    const runner = new Runner({
-        pipeline: [
-            remove(distPath),
-            remove(appRootPath),
-            runCommand({
-                command: 'npm',
-                args: ['run', 'build'],
-                cwd: rootMonoRepoPath
-            }),
-            runCommand({
-                command: 'cordova',
-                args: ['create', appName],
-                cwd: __dirname
-            }),
-            copy(path.join(__dirname, 'test', 'template'), appPath),
-            copy(distPath, appPath),
-            copy(
-                shimTestsPath,
-                appTestsPath
-            ),
-            copy(
-                commonTestsPath,
-                appTestsPath
-            ),
-            processTemplateFile(
-                path.join(appPath, 'index.template.hbs'),
-                () => ({
-                    tests: walk(path.join(appPath, 'tests'), {
-                        nodir: true
-                    }).map(f => `./${path.relative(appPath, f.path)}`),
-                    logServerPort
-                }),
-                path.join(appPath, 'index.html')
-            ),
-            copyTestRunner(appPath),
-            runCommand({
-                command: 'cordova',
-                args: ['platform', 'add', osName],
-                cwd: appRootPath
-            })
+  const runner = new Runner({
+    pipeline: [
+      logServer(),
+      remove(distPath),
+      remove(appRootPath),
+      runCommand({
+        command: 'npm',
+        args: ['run', 'build'],
+        cwd: rootMonoRepoPath
+      }),
+      runCommand({
+        command: 'cordova',
+        args: ['create', appName],
+        cwd: __dirname
+      }),
+      copy(path.join(__dirname, 'test', 'template'), appPath),
+      copy(distPath, appPath),
+      copy(
+          shimTestsPath,
+          appTestsPath
+      ),
+      copy(
+        commonTestsPath,
+        appTestsPath
+      ),
+      processTemplateFile(
+        path.join(appPath, 'index.template.hbs'),
+        () => ({
+          tests: walk(path.join(appPath, 'tests'), {
+            nodir: true
+          }).map(f => `./${path.relative(appPath, f.path)}`),
+          logServerPort
+        }),
+        path.join(appPath, 'index.html')
+      ),
+      copyTestRunner(appPath),
+      when(() => osName === 'android', runCommand({
+        command: 'adb',
+        args: [
+          'reverse',
+          () => `tcp:${logServerPort}`,
+          () => `tcp:${logServerPort}`
         ]
-    });
+      })),
+      runCommand({
+        command: 'cordova',
+        args: ['platform', 'add', osName],
+        cwd: appRootPath
+      }),
+      ...[
+        'https://github.com/apache/cordova-plugin-file.git',
+        'https://github.com/apache/cordova-plugin-whitelist',
+        'https://github.com/apache/cordova-plugin-file-transfer.git',
+        'cordova-sqlite-storage'
+      ].map(p => {
+        return runCommand({
+          command: 'cordova',
+          args: ['plugin', 'add', '--force', p],
+          cwd: appRootPath
+        });
+      }),
+      runCommand({
+        command: 'cordova',
+        args: ['run', osName],
+        cwd: appRootPath
+      })
+    ]
+  });
 
 
-    runner.on('log.start', port => (logServerPort = port));
+  runner.on('log.start', port => (logServerPort = port));
 
-    runner
-        .run()
-        .then(() => console.log('done'))
-        .catch(err => console.log(err));
+  runner
+    .run()
+    .then(() => console.log('done'))
+    .catch(err => console.log(err));
 }
 
 module.exports = runPipeline;
